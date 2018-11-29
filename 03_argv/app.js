69ad4eb7db7302dc191450851032bc5b2c874d2f
evb0110 <7149553@gmail.com>
2018-11-30 00:28:03 +0300

    add stubs for functions called for different commands

--- 03_argv/app.js
+++ 03_argv/app.js
@@ -7,4 +7,17 @@ const notes = require('./notes.js');
 
 const argv = yargs.argv;
 
-notes.addNote(argv.title, argv.body);
+let command = argv._[0];
+
+if (command === "add") {
+  notes.addNote(argv.title, argv.body);
+} else if (command === "list") {
+  notes.listNotes();
+} else if (command === "del") {
+  notes.delNote(argv.title);
+} else if (command === "show") {
+  notes.showNote(argv.title)
+} else {
+  console.log(`Unable to recognize command ${argv._[0]}`)
+}
+
--- 03_argv/notes.js
+++ 03_argv/notes.js
@@ -1,7 +1,23 @@
 const addNote = (title, body) => {
-  console.log(`Adding note with title "${title}" and body "${body}"`);
+  console.log(`Adding note with title` +
+              ` "${title}" and body "${body}"`);
+};
+
+const listNotes = () => {
+  console.log(`Listing all the notes`);
+};
+
+const delNote = (title) => {
+  console.log(`Deleting note with title "${title}"`);
+}
+
+const showNote = (title) => {
+  console.log(`Showing note with title "${title}"`)
 }
 
 module.exports = {
-  addNote
+  addNote,
+  listNotes,
+  delNote,
+  showNote,
 };
