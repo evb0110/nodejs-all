4621acc06c34eea10c2daab9926fe662599fde8b
Eugene Barsky <34789976+evb0110@users.noreply.github.com>
2019-02-15 16:58:22 +0300

    rendering with ejs

--- EXPRESS_COURSE/04_rendering/rendering.js
+++ EXPRESS_COURSE/04_rendering/rendering.js
@@ -1,5 +1,9 @@
+const path = require('path');
 const express = require('express');
 const app = express();
+
+const helmet = require('helmet');
+app.use(helmet());
 const port = 3000;
 
 app.use(express.static('public'));
@@ -7,8 +11,17 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded());
 
-app.get('/', (req,res, next) => {
-  res.send('Sanity check!')
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+app.get('/', (req, res, next) => {
+  // res.send('<h1>Sanity check!</h1>')
+  // res.json({
+  //   msg: "Success!"
+  // })
+  res.render('index');
+
 });
 
 app.listen(3000);
