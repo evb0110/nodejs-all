998e460668e4d3e7d616f95566bc99b8e3a37d58
Eugene Barsky <34789976+evb0110@users.noreply.github.com>
2021-06-19 22:22:54 +0300

    misc

--- 04_json/app1.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-
-let personNewString = fs.readFileSync('persons.json', "utf8");
-
-
-let personObjectNew = JSON.parse(personNewString);
-console.log(personObjectNew.pets.cat);
